5fa371d463b1989f9c448d598d4f7630977b1e20
M. Bereket <bmuluken804@gmail.com>
2026-03-08 21:13:29 +0300

    Create Book object constructor and addBookToLibrary function

---
+++ static/js/script.js
@@ -0,0 +1,27 @@
+let myLibrary = [];
+
+function Book(title, author, bookStat){
+    if(!new.target){
+        throw error("Use 'new' to create an object!")
+    }
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.bookStat = bookStat;
+}
+book = new Book("My Book", "me", "Read");
+myLibrary.push(book);
+
+function addBookToLibrary(){
+    title = prompt("title: ");
+    author = prompt("author: ");
+    bookStat = prompt("Have you read the book?: ");
+    newBook = new Book(1, title, author, bookStat);
+    myLibrary.push(newBook)
+    return;
+}
+
+addBook = document.querySelector(".add");
+addBook.addEventListener("click", () =>{
+    addBookToLibrary();
+});
