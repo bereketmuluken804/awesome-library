92c73b33d715c9fc84795f1b64f5027a7d605ec6
M. Bereket <bmuluken804@gmail.com>
2026-03-08 23:19:17 +0300

    Render books from myLibrary array plus add new books through prompt

--- static/js/script.js
+++ static/js/script.js
@@ -16,8 +16,24 @@ function addBookToLibrary(){
     title = prompt("title: ");
     author = prompt("author: ");
     bookStat = prompt("Have you read the book?: ");
-    newBook = new Book(1, title, author, bookStat);
-    myLibrary.push(newBook)
+    newBook = new Book(title, author, bookStat);
+    myLibrary.push(newBook);
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card")
+    bookCard.setAttribute("id", newBook.id)
+    bookCard.innerHTML = `
+                    <div class="book-content">
+                        <h2 class="book-title">${newBook.title}</h2>
+                        <h3 class="book-author">By: ${newBook.author}</h3>
+                        <p class="book-stat">${newBook.bookStat}</p>
+                    </div>
+                    <div class="controls">
+                        <button class="read-mark">Mark Unread</button>
+                        <button class="edit">Edit</button>
+                        <button class="remove-book">Remove</button>
+                    </div>
+    `
+    bookContainer.appendChild(bookCard);
     return;
 }
 
@@ -26,5 +42,24 @@ addBook.addEventListener("click", () =>{
     addBookToLibrary();
 });
 
-bookContainer = document.querySelector(".book-container");
-bookdocument.createElement("div");
+const bookContainer = document.querySelector(".book-container");
+
+// displaying books inside library
+for (book of myLibrary){
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card")
+    bookCard.setAttribute("id", book.id)
+    bookCard.innerHTML = `
+                    <div class="book-content">
+                        <h2 class="book-title">${book.title}</h2>
+                        <h3 class="book-author">By: ${book.author}</h3>
+                        <p class="book-stat">${book.bookStat}</p>
+                    </div>
+                    <div class="controls">
+                        <button class="read-mark">Mark Unread</button>
+                        <button class="edit">Edit</button>
+                        <button class="remove-book">Remove</button>
+                    </div>
+    `
+    bookContainer.appendChild(bookCard);
+}
