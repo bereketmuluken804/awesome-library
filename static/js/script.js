917b953a39fb6868dbc2bca2c6747bff9b02674b
M. Bereket <bmuluken804@gmail.com>
2026-03-09 20:04:14 +0300

    add dialog for adding a book

--- static/js/script.js
+++ static/js/script.js
@@ -1,36 +1,37 @@
 let myLibrary = [];
 
-function Book(title, author, bookStat){
+function Book(title, author, pages, bookStat){
     if(!new.target){
         throw error("Use 'new' to create an object!")
     }
     this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
+    this.pages = pages
     this.bookStat = bookStat;
 }
-book = new Book("My Book", "me", "Read");
+book = new Book("My Book", "me", 120, "Read");
 myLibrary.push(book);
 
 function addBookToLibrary(){
-    title = prompt("title: ");
-    author = prompt("author: ");
-    bookStat = prompt("Have you read the book?: ");
+    // addForm = document.querySelector("form");
+    // addForm.style["z-index"]= 2;
+
     newBook = new Book(title, author, bookStat);
     myLibrary.push(newBook);
     const bookCard = document.createElement("div");
     bookCard.classList.add("book-card")
-    bookCard.setAttribute("id", newBook.id)
     bookCard.innerHTML = `
                     <div class="book-content">
                         <h2 class="book-title">${newBook.title}</h2>
                         <h3 class="book-author">By: ${newBook.author}</h3>
+                        <p calss="book-page">Pages: ${newBook.pages}</p>
                         <p class="book-stat">${newBook.bookStat}</p>
                     </div>
                     <div class="controls">
                         <button class="read-mark">Mark Unread</button>
                         <button class="edit">Edit</button>
-                        <button class="remove-book">Remove</button>
+                        <button class="remove-book" id=${newBook.id}>Remove</button>
                     </div>
     `
     bookContainer.appendChild(bookCard);
@@ -39,6 +40,7 @@ function addBookToLibrary(){
 
 addBook = document.querySelector(".add");
 addBook.addEventListener("click", () =>{
+    dialog.showModal();
     addBookToLibrary();
 });
 
@@ -48,12 +50,12 @@ const bookContainer = document.querySelector(".book-container");
 for (book of myLibrary){
     const bookCard = document.createElement("div");
     bookCard.classList.add("book-card")
-    bookCard.setAttribute("id", book.id)
     bookCard.innerHTML = `
                     <div class="book-content">
                         <h2 class="book-title">${book.title}</h2>
                         <h3 class="book-author">By: ${book.author}</h3>
-                        <p class="book-stat">${book.bookStat}</p>
+                        <p calss="book-page">Pages: ${book.pages}</p>
+                        <p class="book-stat" id=${book.id}>${book.bookStat}</p>
                     </div>
                     <div class="controls">
                         <button class="read-mark">Mark Unread</button>
@@ -63,3 +65,16 @@ for (book of myLibrary){
     `
     bookContainer.appendChild(bookCard);
 }
+
+const dialog = document.querySelector("dialog");
+const cancel = document.querySelector(".cancel-btn");
+
+cancel.addEventListener('click', (e)=>{
+    dialog.close()
+})
+add
+const removeBtn = document.querySelector(".remove-book");
+removeBtn.addEventListener("click", (e)=>{
+    id = e.targed.id;
+    
+}); 
